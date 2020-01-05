e18d0299105e04392553a18f1b34d0597d1cacc7
Felix <57230205+FelixApocrypha@users.noreply.github.com>
2020-01-05 22:35:35 +0800

    DuLinkList
    先挖个坑，回头填上

---
+++ Double Linked List/Double Linked List.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef int ElemType;
+typedef struct DuLNode
+{
+	ElemType data;//数据域
+	DuLNode* prior;//直接前驱
+	DuLNode* next;//直接后继
+}DuLNode,*DuLinkList;
+
+int main()
+{
+}
