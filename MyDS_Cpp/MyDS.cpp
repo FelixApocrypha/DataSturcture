d023de96e5b09fa36be9a2acc29cadb24ffaf944
Felix <felix2000@foxmail.com>
2021-03-22 19:51:23 +0800

    bugfix

--- MyDS_Cpp/MyDS.cpp
+++ MyDS_Cpp/MyDS.cpp
@@ -5,7 +5,7 @@ int main()
 {
     MyDS::SeqList<int> sq1 = {1,2,3};
     MyDS::SeqList<int> sq2(sq1);
-    std::cout << ++sq1.At(2) << "\n";
+    std::cout << sq1.At(2) << "\n";
     std::cout << sq2.At(2) << "\n";
 
     std::cout << "This is My Data Sturcture of Cpp.\n";
